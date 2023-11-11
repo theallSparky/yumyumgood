77908906eb1f87d3b3ec99286790d75023c34123
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-11-11 17:17:36 -0600

    Added top left for topnav component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,13 @@
-import  TopNav  from './components/TopNav'
+import TopNav from './components/TopNav'
+import Footer from './components/Footer'
+import './index.css'
 
 function App() {
 
   return (
     <div className='App'>
       <TopNav />
+      <Footer />
     </div>
   )
 }
--- client/src/components/TopNav.jsx
+++ client/src/components/TopNav.jsx
@@ -2,12 +2,18 @@ import React from 'react'
 import { AiOutlineMenu, AiOutlineSearch, AiOutlineClose, AiFillTag } from 'react-icons/ai'
 
 const TopNav = () => {
-    console.log('hello')
   return (
     <div className='max-w-[1520] mx-auto flex justify-between items-center p-4'>
-        <div className='flex items-center'>
+        <div className='flex flex-row items-center'>
             <div className='cursor-pointer'>
                 <AiOutlineMenu size={25} />
+            </div>
+            <h1 className='text-2xl sm:text-3xl lg:text-4xl px-2'>YumYum
+                <span>Good</span>
+            </h1>
+            <div className='hidden lg:flex items-center bg-gray-200 rounded-full p-1 text-[14px]'>
+                <p className='bg-orange-700 text-white rounded-full p-2 text-bold'>Free</p>
+                <p className='p-2 text-bold'>Delivery</p>
             </div>
         </div>
     </div>
