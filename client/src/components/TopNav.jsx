0ee89028be2c15b9c668808467c712bef2ab4b66
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-11-11 17:24:47 -0600

    Created the search icon top right

--- client/src/components/TopNav.jsx
+++ client/src/components/TopNav.jsx
@@ -4,7 +4,7 @@ import { AiOutlineMenu, AiOutlineSearch, AiOutlineClose, AiFillTag } from 'react
 const TopNav = () => {
   return (
     <div className='max-w-[1520] mx-auto flex justify-between items-center p-4'>
-        <div className='flex flex-row items-center'>
+        <div className='flex items-center'>
             <div className='cursor-pointer'>
                 <AiOutlineMenu size={25} />
             </div>
@@ -15,6 +15,9 @@ const TopNav = () => {
                 <p className='bg-orange-700 text-white rounded-full p-2 text-bold'>Free</p>
                 <p className='p-2 text-bold'>Delivery</p>
             </div>
+        </div>
+        <div className='bg-gray-200 rounded-full flex items-center px-2 w-[200px] sm:w-[400px] lg:w[500px]'>
+            <AiOutlineSearch size={25} />
         </div>
     </div>
   )
