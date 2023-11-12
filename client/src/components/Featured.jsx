904a9dbecb7fabce9e1c098abbe8d7de2db24a01
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-11-12 13:24:46 -0600

    Added left and right arrows for Featured component

--- client/src/components/Featured.jsx
+++ client/src/components/Featured.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs'
 
 const Featured = () => {
     const sliders = [
@@ -18,6 +19,12 @@ const Featured = () => {
         <div className='w-full h-full rounded-2xl bg-center bg-cover duration-500'
                 style={{backgroundImage: `url(${sliders[currentIndex].url})`}}
         ></div>
+        <div className='absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-orange-700 text-white cursor-pointer'>
+            <BsChevronCompactLeft />
+        </div>
+        <div className='absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-orange-700 text-white cursor-pointer'>
+            <BsChevronCompactRight />
+        </div>
     </div>
   )
 }
