d3b5d45334ac7b705f6ae88b951d4f5193132ec7
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-11-12 15:27:49 -0600

    Used RxDotFilled to add slider tracker for Featured

--- client/src/components/Featured.jsx
+++ client/src/components/Featured.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs'
+import { RxDotFilled } from 'react-icons/rx'
 
 const Featured = () => {
     const sliders = [
@@ -27,6 +28,10 @@ const Featured = () => {
         setCurrentIndex(newIndex)
     }
 
+    const  moveToSlide = (slideIndex) => {
+        setCurrentIndex(slideIndex)
+    }
+
   return (
     <div className='max-w-[1520px] h-[500px] w-full py-4 px-4 relative group'>
         <div className='w-full h-full rounded-2xl bg-center bg-cover duration-500'
@@ -37,6 +42,16 @@ const Featured = () => {
         </div>
         <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-orange-700 text-white cursor-pointer'>
             <BsChevronCompactRight onClick={nextSlider}/>
+        </div>
+        <div className='flex top-4 justify-center py-2'>
+            {
+                sliders.map((sliderItems, slideIndex) => (
+                    <div className='text-2xl cursor-pointer'>
+                        <RxDotFilled/>
+                    </div>
+
+                ))
+            }
         </div>
     </div>
   )
