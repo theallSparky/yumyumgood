09e56a803a8b411e7a224a1f1005221a209aadd8
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-11-12 15:17:04 -0600

    Added onClick to move slider for left arrow

--- client/src/components/Featured.jsx
+++ client/src/components/Featured.jsx
@@ -14,13 +14,21 @@ const Featured = () => {
         }
     ]
     const [currentIndex, setCurrentIndex] = useState(0)
+
+    const prevSlider = () => {
+        const isFirstSlide = currentIndex === 0
+        const newIndex = isFirstSlide ? sliders.length - 1 : currentIndex -1
+        setCurrentIndex(newIndex)
+    }
+
+
   return (
     <div className='max-w-[1520px] h-[500px] w-full py-4 px-4 relative group'>
         <div className='w-full h-full rounded-2xl bg-center bg-cover duration-500'
                 style={{backgroundImage: `url(${sliders[currentIndex].url})`}}
         ></div>
         <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-orange-700 text-white cursor-pointer'>
-            <BsChevronCompactLeft />
+            <BsChevronCompactLeft onClick={prevSlider}/>
         </div>
         <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-orange-700 text-white cursor-pointer'>
             <BsChevronCompactRight />
