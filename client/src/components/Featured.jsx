44cef9889d05f60c4928e44990b44d88de5f015c
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-11-12 15:13:26 -0600

    Made Featured side arrows appear on hover only

--- client/src/components/Featured.jsx
+++ client/src/components/Featured.jsx
@@ -15,14 +15,14 @@ const Featured = () => {
     ]
     const [currentIndex, setCurrentIndex] = useState(0)
   return (
-    <div className='max-w-[1520px] h-[500px] w-full py-4 px-4 relative'>
+    <div className='max-w-[1520px] h-[500px] w-full py-4 px-4 relative group'>
         <div className='w-full h-full rounded-2xl bg-center bg-cover duration-500'
                 style={{backgroundImage: `url(${sliders[currentIndex].url})`}}
         ></div>
-        <div className='absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-orange-700 text-white cursor-pointer'>
+        <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-orange-700 text-white cursor-pointer'>
             <BsChevronCompactLeft />
         </div>
-        <div className='absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-orange-700 text-white cursor-pointer'>
+        <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-orange-700 text-white cursor-pointer'>
             <BsChevronCompactRight />
         </div>
     </div>
