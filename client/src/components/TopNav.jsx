39a14b7d39a24435da713d6bfd06327d7e1a3bf5
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-11-11 21:32:09 -0600

    Made usestate apply to menu button

--- client/src/components/TopNav.jsx
+++ client/src/components/TopNav.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { AiOutlineMenu, AiOutlineSearch, AiOutlineClose, AiFillTag } from 'react-icons/ai'
 import { BsFillCartFill, BsPerson} from 'react-icons/bs'
 
 const TopNav = () => {
+    const [sideNav, setSideNav] = useState(false)
+    console.log(sideNav)
   return (
     <div className='max-w-[1520] mx-auto flex justify-between items-center p-4'>
         <div className='flex items-center'>
-            <div className='cursor-pointer'>
+            <div onClick={()=>setSideNav(!sideNav)} className='cursor-pointer'>
                 <AiOutlineMenu size={25} />
             </div>
             <h1 className='text-2xl sm:text-3xl lg:text-4xl px-2'>YumYum
