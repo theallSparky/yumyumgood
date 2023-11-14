bfc3a91894ac29589a0a90c2afdff3d7bd2ba8e8
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-11-14 13:34:29 -0600

    Within Meal component, added padding and item.id as key

--- client/src/components/Meal.jsx
+++ client/src/components/Meal.jsx
@@ -6,21 +6,22 @@ const Meal = () => {
   return (
     <div className='max-w-[1520px] m-auto px-2 py-12'>
         <h1 className='text-orange-500 font-bold text-2xl text-center py-2'>Our Meal</h1>
-        <div className='grid md:grid-cols-2 sm:grid-cols-1 lg:grid-cols-4 gap-6'>
+        <div className='grid md:grid-cols-2 sm:grid-cols-1 lg:grid-cols-4 gap-6 py-4'>
             {
                 mealData.map((item)=>(
-                    <div className='border-none hover:scale-105 duration-300'>
+                    <div key={item.id}
+                    className='border-none hover:scale-105 duration-300'>
                         <img 
                             src={item.image}
                             alt={item.name}
                             className='w-full h-[200px] object-cover rounded-lg'
                         />
-                        <div className='flex justify-between py-2 px-4'>
+                        <div className='flex justify-between py-2 px-2'>
                             <p className='font-bold'>{item.name}</p>
                             <p className='bg-orange-700 h-18 w-18 rounded-full -mt-10 text-white py-4 px-2 border-8'>{item.price}</p>
                         </div>
                         <div className='pl-2 py-4'>
-                            <p>View More<ArrowSmRightIcon className='w-5 ml-2'/></p>
+                            <p className='flex items-center text-indigo-600 -mt-8'>View More<ArrowSmRightIcon className='w-5 ml-2'/></p>
                         </div>
                     </div>
                 ))
