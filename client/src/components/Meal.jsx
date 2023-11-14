08d20b32c9f270e4ca1162ddb09032b8c8b433e5
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-11-14 13:45:55 -0600

    Replaced mealData mapping with useState foods

--- client/src/components/Meal.jsx
+++ client/src/components/Meal.jsx
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { mealData } from '../data/data'
 import {ArrowSmRightIcon} from '@heroicons/react/outline'
 
 const Meal = () => {
-  return (
+    const [foods, setFoods] = useState(mealData)
+    return (
     <div className='max-w-[1520px] m-auto px-2 py-12'>
-        <h1 className='text-orange-500 font-bold text-2xl text-center py-2'>Our Meal</h1>
+        <h1 className='text-orange-500 font-bold text-2xl text-center py-2'>
+            Our Meal
+        </h1>
+        <div className='flex flex-col lg:flex-row justify-center'>
+            <div className='flex justify-center md:justify-center'>
+                <button className='m-1 border-orange-700 text-white bg-orange-700 hover:bg-white hover:text-orange-700'>All</button>
+                <button className='m-1 border-orange-700 text-white bg-orange-700 hover:bg-white hover:text-orange-700'>Pizza</button>
+                <button className='m-1 border-orange-700 text-white bg-orange-700 hover:bg-white hover:text-orange-700'>Chicken</button>
+                <button className='m-1 border-orange-700 text-white bg-orange-700 hover:bg-white hover:text-orange-700'>Salad</button>
+            </div>
+        </div>
         <div className='grid md:grid-cols-2 sm:grid-cols-1 lg:grid-cols-4 gap-6 py-4'>
             {
-                mealData.map((item)=>(
+                foods.map((item)=>(
                     <div key={item.id}
                     className='border-none hover:scale-105 duration-300'>
                         <img 
