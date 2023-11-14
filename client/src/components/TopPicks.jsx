e16d05f03245a15e54016c13d34984455a213cf4
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-11-13 23:27:01 -0600

    Installed and imported splide

--- client/src/components/TopPicks.jsx
+++ client/src/components/TopPicks.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Splide, SplideSlide } from '@splidejs/react-splide'
+import '@splidejs/react-splide/css'
 
 const TopPicks = () => {
   return (
     <>
         <h1 className='text-orange-500 font-bold text-2xl text-center py-2'>Top Picks</h1>
-        
+
     </>
   )
 }
