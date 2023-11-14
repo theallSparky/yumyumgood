d6e23c881d3ecb93e598b54857fea04862d9224e
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-11-13 23:01:03 -0600

    Finished basic styling for Delivery component

--- client/src/components/Delivery.jsx
+++ client/src/components/Delivery.jsx
@@ -4,8 +4,15 @@ const Delivery = () => {
   return (
     <div className='w-full bg-white py-16 px-4'>
         <h3 className='text-orange-500 font-bold text-2xl text-center'>Quick Delivery App</h3>
-        <div className='w-[1520px] mx-auto grid md:grid-cols-2'></div>
-        <img className='w-[550px] mx-auto my-4' src='https://res.cloudinary.com/ehizeex-shop/image/upload/v1672676822/NetflixApp/FC_two_phones.6ec9a842f905769677f9_m91off.webp' />
+        <div className='w-[1240px] mx-auto grid md:grid-cols-2'>
+            <img className='w-[550px] mx-auto my-4' src='https://res.cloudinary.com/ehizeex-shop/image/upload/v1672676822/NetflixApp/FC_two_phones.6ec9a842f905769677f9_m91off.webp' />
+            <div className='flex flex-col justify-center'>
+                <p className='text-[#00df9a] font-bold'>Get The App!</p>
+                <h1 className='md:text-4xl sm:text-3xl font-bold py-2'>Limitless Convenience on-demand!</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <button className='bg-black text-[#00df9a] w-[200px] rounded-medium my-6 mx-auto md:mx-0 py-3'>Get Started</button>
+            </div>
+        </div>
     </div>
   )
 }
