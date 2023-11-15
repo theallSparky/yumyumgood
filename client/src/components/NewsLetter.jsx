7474afb1d242afe04e92da33e4fc197f4067609d
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-11-15 00:21:30 -0600

    Finished up basic needs for NewsLetter component

--- client/src/components/NewsLetter.jsx
+++ client/src/components/NewsLetter.jsx
@@ -11,8 +11,19 @@ const NewsLetter = () => {
             </div>
             <div className='my-4'>
                 <div className='flex flex-col sm:flex-row items-center justify-between w-full'>
-                    <input />
+                    <input 
+                    type="email"
+                    placeholder="email@example.test"
+                    className='p-3 flex w-full rounded-md text-black'
+                    />
+                    <button className='bg-[#00df9a] text-white rounded-md font-medium w-[200px] ml-4 my-6 px-6 py-3 border-none'>
+                        Notify Me
+                    </button>
                 </div>
+                <p>we are concerned about the security of your data, Read{""}
+                <span className='text-[#00df9a]'>Privacy Policy</span>
+                </p>
+                <hr className='my-8 bg-gray-700 border-1' />
             </div>
         </div>
     </div>
